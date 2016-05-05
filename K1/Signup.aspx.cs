f956b26ea5a933875e4f13fd3d74122d2d6130ad
karthikr1901 <karthikr1901@gmail.com>
2016-05-05 18:09:44 +0800

    Sign up validation1

--- K1/Signup.aspx.cs
+++ K1/Signup.aspx.cs
@@ -34,7 +34,46 @@ public partial class Signup : System.Web.UI.Page
         //TextBox7.Enabled = false;
 
         genpass();
-    }    
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //TextBox9.Text = TextBox6.Text + Label9.Text + DropDownList1.SelectedItem.Text;
+        if (check())
+        {
+            if (isValidSubmit())
+            {
+                cn.Open();
+                cmd.CommandText = "Insert into Customer values ('" + txtName.Text + "','" + txtAddress.Text + "','" + txtCity.Text + "', '" + txtMobile.Text + "','" + Label9.Text + "' , '" + txtEmail.Text + "')";
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                txtName.Enabled = false;
+                txtAddress.Enabled = false;
+                txtCity.Enabled = false;
+                txtMobile.Enabled = false;
+                txtEmail.Enabled = false;
+                btnSubmit.Enabled = false;
+                lblErrorMessage.Text = "Account created successfully";
+                Label9.Visible = true;
+                Label8.Visible = true;
+                btnOK.Visible = true;
+            }
+
+            //NetworkCredential loginInfo = new NetworkCredential("smsmarartt@gmail.com", "ttraramsms"); // give your email id and password here.. 
+            //MailMessage msg = new MailMessage();
+            //msg.From = new MailAddress(TextBox8.Text);
+            //msg.To.Add(new MailAddress(TextBox6.Text));
+            //msg.Subject = "Password for the account you have created";
+            //msg.Body = "Your Password: " + TextBox7.Text;
+            //msg.IsBodyHtml = true;
+            //SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            //client.EnableSsl = true;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = loginInfo;
+            //client.Send(msg);
+            cn.Close();
+            //}
+        }
+    }
 }
 
 
