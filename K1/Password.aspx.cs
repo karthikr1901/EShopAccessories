d25651d337db66f5abdcf6590ea07bfe80bb313f
karthikr1901 <karthikr1901@gmail.com>
2016-05-09 23:18:54 +0800

    Password retrieve and display

--- K1/Password.aspx.cs
+++ K1/Password.aspx.cs
@@ -15,6 +15,44 @@ public partial class Password : System.Web.UI.Page
     SqlConnection cn = new SqlConnection(WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
     SqlCommand cmd = new SqlCommand();
 
+    private void check()
+    {
+        //cn.ConnectionString = "Data Source = 172.23.197.6\\sql2008; Initial Catalog = Smart Accessories Inc.; User ID = sa; Password = PASSWORD@123";
+        cn.Open();
+
+
+        cmd.CommandText = "select * from Customer where Email = '" + txtEmail.Text + "'and Phone = '" + txtMobile.Text + "'";
+        cmd.Connection = cn;
+        SqlDataReader DR2 = cmd.ExecuteReader();
+
+        if (DR2.Read())
+        {
+            Label5.Text = "Your password is :" + DR2.GetValue(5).ToString();
+
+
+
+
+           
+            //NetworkCredential loginInfo = new NetworkCredential("smsmarartt@gmail.com", "ttraramsms"); // give your email id and password here.. 
+            //MailMessage msg = new MailMessage();
+            //msg.From = new MailAddress(TextBox4.Text);
+            //msg.To.Add(new MailAddress(TextBox1.Text));
+            //msg.Subject = TextBox2.Text;
+            //msg.Body = Label5.Text;
+            //msg.IsBodyHtml = true;
+            //SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            //client.EnableSsl = true;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = loginInfo;
+            //client.Send(msg);
+            //Response.Write("Password has been sent to your email");
+        }
+        else
+        {
+            Label5.Text = "Invalid Email ID or Phone Number!!!";
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
         TextBox2.Visible = false;
