896734c052e258edeadca2fe03490bb2dbad855f
karthikr1901 <karthikr1901@gmail.com>
2016-05-11 21:19:47 +0800

    Change password verify

--- K1/Change.aspx.cs
+++ K1/Change.aspx.cs
@@ -22,5 +22,28 @@ public partial class Change : System.Web.UI.Page
     {
         check();
     }
+    private void check()
+    {
+        cn.Open();
+        cmd.CommandText = "select * from Customer where Email = '" + txtEmail.Text + "' and Password = '" + TextBox3.Text + "' and Phone = '" + txtMobile.Text + "'";
+        cmd.Connection = cn;
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        if (dt.Rows.Count > 0)
+        {
+            if(TextBox4.Text == TextBox5.Text)
+            {
+                SqlCommand cm = new SqlCommand("update Customer set Password ='" + TextBox4.Text + "' where Email ='" + txtEmail.Text + "' and Phone = '" + txtMobile.Text + "'");
+                cm.Connection = cn;
+                cm.ExecuteNonQuery();
+                Label7.Text = "Password changed!!!!";
+            }
+            else
+                Label7.Text = "Both New Password and Confirm Password are not matching!!!!";
+        }
+        else
+            Label7.Text = "Wrong entry!!!!";            
+    }
 }
     
