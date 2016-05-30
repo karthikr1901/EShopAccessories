2f09a2b88f67ce10f75914c7466e52836c7eb56c
karthikr1901 <karthikr1901@gmail.com>
2016-05-30 18:45:24 +0800

    Detail of prod start

--- K1/detail.aspx.cs
+++ K1/detail.aspx.cs
@@ -12,6 +12,13 @@ using System.Web.Configuration;
 
 public partial class detail : System.Web.UI.Page
 {
+    SqlConnection cn = new SqlConnection(WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+    SqlCommand cmd = new SqlCommand();
+    //SqlDataAdapter da;
+    //DataTable dt;
+    SqlCommand cm;
+    string productid = "", userid = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
         
