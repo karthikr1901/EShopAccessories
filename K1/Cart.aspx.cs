12c91f90baca74a2850dd6598c3f01072b9bd537
karthikr1901 <karthikr1901@gmail.com>
2016-07-05 23:41:39 +0800

    Validation for payment v1.4

--- K1/Cart.aspx.cs
+++ K1/Cart.aspx.cs
@@ -120,6 +120,11 @@ public partial class Cart : System.Web.UI.Page
                     lblErrorMessage.Text = "Please Enter Card Number";
                     return false;
                 }
+                if (TextBox11.Text.Length != 4 || TextBox12.Text.Length != 4 || TextBox13.Text.Length != 4 || TextBox14.Text.Length != 4 || TextBox15.Text.Length != 3)
+                {
+                    lblErrorMessage.Text = "Please Enter Card Number";
+                    return false;
+                }
             }
             
         }
