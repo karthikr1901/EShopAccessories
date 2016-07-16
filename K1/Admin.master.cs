cab7c88634e23e4296adb1d5cc021463c4378ba7
karthikr1901 <karthikr1901@gmail.com>
2016-07-15 18:47:19 -0700

    Admin master page v1.0

---
+++ K1/Admin.master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Web.Configuration;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    
+}
+
