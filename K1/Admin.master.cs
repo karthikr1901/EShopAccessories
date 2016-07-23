5c70002d5c3db957a57658d8e159d508ce142b45
karthikr1901 <karthikr1901@gmail.com>
2016-07-24 02:21:20 +0800

    Admin master page v3.0

--- K1/Admin.master.cs
+++ K1/Admin.master.cs
@@ -30,6 +30,11 @@ public partial class MasterPage : System.Web.UI.MasterPage
             LinkButton13.Visible = true;
             sc.Visible = LinkButton2.Visible = false;
         }
+        if ((string)Session["K"] == "10")
+        {
+            LinkButton13.Visible = true;
+            sc.Visible = LinkButton2.Visible = false;
+        }
     }    
 }
 
