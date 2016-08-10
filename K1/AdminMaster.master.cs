74070f60ac58f0addaa5b631ca35f70cd02db13e
karthikr1901 <karthikr1901@gmail.com>
2016-08-10 23:59:14 +0800

    Admin master page v2 page load v1.0

--- K1/AdminMaster.master.cs
+++ K1/AdminMaster.master.cs
@@ -12,5 +12,9 @@ public partial class AdminMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        cn.Open();
+        if (Session["K"] != null)
+        {
+        }
     }
 }
