2d386cee50d35db0cbcf71a28f8d0e0be9b9cd86
karthikr1901 <karthikr1901@gmail.com>
2016-11-05 00:32:04 +0800

    Users pg v1 sql connection

--- K1/Users.aspx.cs
+++ K1/Users.aspx.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
 
 public partial class Users : System.Web.UI.Page
 {
+    SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
     protected void Page_Load(object sender, EventArgs e)
     {
     }
